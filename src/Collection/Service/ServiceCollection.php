1bbfd9726fd09b070a213bc4487a8a393fc331e4
nxs_bertram <bertram@nexus-netsoft.com>
2017-04-30 22:32:13 +0200

    + Collections

---
+++ src/Collection/Service/ServiceCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mibexx\dockercomposefile\Collection\Service;
+
+
+use mibexx\collection\Collection\Collection;
+use mibexx\collection\Collection\CollectionInterface;
+
+class ServiceCollection extends Collection implements CollectionInterface
+{
+}
---
+++ src/Collection/Service/ServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mibexx\dockercomposefile\Collection\Service;
+
+
+use mibexx\collection\Collection\CollectionInterface;
+
+interface ServiceInterface extends CollectionInterface
+{
+
+}
